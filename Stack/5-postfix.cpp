22055a626e403b0fec3f311f8af43bd2e944eebb
hgc-qwe <1410919772@qq.com>
2026-04-27 17:38:30 +0800

    04.27

---
+++ Stack/5-postfix.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int func(string& exp) {
+    stack<int> s;
+    for (char c : exp) {
+        if (c == ' ') {
+            continue;
+        } else if (c != '+' && c != '-' && c != '*' && c != '/') {
+            s.push(c - '0');
+        } else {
+            int op1 = s.top();
+            s.pop();
+            int op2 = s.top();
+            s.pop();
+            int res;
+            if (c == '+') {
+                res = op1 + op2;
+            } else if (c == '-') {
+                res = op2 - op1;
+            } else if (c == '*') {
+                res = op1 * op2;
+            } else {
+                res = op2 / op1;
+            }
+            s.push(res);
+        }
+    }
+    return s.top();
+}
+
+int main() {
+    string exp;
+    getline(cin, exp);
+    int res = func(exp);
+    cout << res << endl;
+
+    return 0;
+}
